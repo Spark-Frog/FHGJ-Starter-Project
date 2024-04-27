ef2450a46fea503f343a0016bab346c460977590
Calandstrom <155859914+Calandstrom@users.noreply.github.com>
2024-04-27 14:00:23 -0400

    Update utils.py
    Minor bug fixes. Read comments for explainations

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import pygame
 
 
-BASE_IMG_PATH = 'data/images/'
+BASE_IMG_PATH = '/Users/carterlandstrom/Downloads/data/images/' 
+#For the BASE_IMG_PATH you will need to change it so it correctly directs to the files.
+#Most likely using vs or vscode just put /data/images/ but since Macs are dumb I need the 
+#extra stuff/
 def load_image(path):
     img = pygame.image.load(BASE_IMG_PATH + path).convert()
     img.set_colorkey((0,0,0))
